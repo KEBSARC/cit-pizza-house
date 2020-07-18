326acd918d1cf6478931cffdc8aea17be4ed62a9
KEBSARC <kebsarc@gmail.com>
2020-07-18 12:08:43 +1200

    Initial commit of pizza ordering app

---
+++ PizzaHouseSchoolProject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg12cit.pizza.house.school.project;
+
+/**
+ *
+ * @author 17065
+ */
+public class PizzaHouseSchoolProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PizzaHouseJFrame m = new PizzaHouseJFrame();
+        m.setVisible(true);
+    }
+    
+}
